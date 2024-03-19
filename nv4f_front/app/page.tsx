a3a3f0c18374bffe3ebda7230f0880ddfc3ef47e
ES, Choi <69956156+escape1001@users.noreply.github.com>
2024-03-19 23:29:51 +0900

    "update" api 테스트

--- nv4f_front/app/page.tsx
+++ nv4f_front/app/page.tsx
@@ -10,15 +10,20 @@ export default async function Home() {
   const posts = await getPosts();
 
   return (
+    <main className="pt-20">
       <div className="center-content">
         <h1>메인 페이지</h1>
-        {
-          posts.map((post, index) => (
-            <li key={index}>
-              {post.title}
-            </li>
-          ))
-        }
+        <ul>
+          {
+            posts.map((post, index) => (
+              <li key={index}>
+                <img src={url+post.thumbnail_image}/>
+                {post.title}
+              </li>
+            ))
+          }
+        </ul>
       </div>
+    </main>
     );
 }
